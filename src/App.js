aa58d6d563bdf77645f11f8ef640bff34c136d38
Emre Memiş <113227617+memisemre@users.noreply.github.com>
2023-01-09 11:25:53 +0300

    Create HomePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Header from './Components/Header';
-
+import Homepage from './Components/Homepage';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Homepage />
     </div>
   );
 }
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,12 +2,14 @@ export default function Header(props) {
   return (
     <div className="header-container">
       <header id="header">
-        <button className="header-language">EN</button>
         <a href="#" target="_blank" className="logo">Lumax</a>
-        <button className="header-login-btn">
+        <button className="header-login-btn" onClick={handleClick}>
           <i className="fa-regular fa-user"></i>Login
         </button>
       </header>
     </div>
   )
+}
+function handleClick(){
+  console.log("osman")
 }
